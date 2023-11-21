f1ce5e6c5cd880a0274ecb540940dd716d020796
guacs <guacs.guacs@gmail.com>
2023-11-21 15:06:16 +0530

    feat: implement the config

---
+++ litestar_svcs/config.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+from typing import Awaitable
+from typing import Callable
+
+from litestar.exceptions import ImproperlyConfiguredException
+
+if TYPE_CHECKING:
+    from svcs import Registry
+
+
+@dataclass(slots=True, frozen=True)
+class SvcsPluginConfig:
+    """The config for the `SvcsPlugin`."""
+
+    registry: Registry | None = None
+    """The `svcs.Registry` instance.
+
+    If provided, this is used and if not a Registry is created with `registry_factory`.
+    """
+
+    registry_factory: (
+        Callable[[], Registry] | Callable[[], Awaitable[Registry]] | None
+    ) = None
+    """A factory function to get registry.
+
+    This is called on app startup if the registry instance has not been
+    provided. This can be async or sync.
+    """
+
+    registry_dependency_key: str = "svcs_registry"
+    """The kwarg name for the `svcs.Registry` to be injected into the dependency callables."""  # noqa: E501
+
+    container_dependency_key: str = "svcs_container"
+    """The kwarg name for the `svcs.Container` to be injected into the dependency callables."""  # noqa: E501
+
+    registry_state_key: str = "svcs_registry"
+    """The key used to store the Registry within the Litestar `State` instance."""
+
+    def __post_init__(self) -> None:
+        if self.registry is None and self.registry_factory is None:
+            msg = "either `registry` or `registry_factory` must be provided"
+            raise ImproperlyConfiguredException(
+                msg,
+            )
+
+        if self.registry is not None and self.registry_factory is not None:
+            msg = "only one of `registry` and `registry_factory` must be provided"
+            raise ImproperlyConfiguredException(
+                msg,
+            )
